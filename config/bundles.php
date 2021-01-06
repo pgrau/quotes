c779ea0548f4a3e380ba4ddb9feb5f5a173200d1
Pau F. Grau <pau.ferran.grau@gmail.com>
2021-01-06 18:54:28 +0100

    configure behat and test api status

--- config/bundles.php
+++ config/bundles.php
@@ -3,5 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    \FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
